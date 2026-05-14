a961c243c16e7b0e0f8ed4a85086fe1b3480a20e
Anisha Yadav <yadavanisha031@gmail.com>
2026-05-14 18:31:14 +0530

    poll entity change

--- lib/views/TPoll_Search/domain/entities/TPollSearchEntity.dart
+++ lib/views/TPoll_Search/domain/entities/TPollSearchEntity.dart
@@ -57,53 +57,28 @@ class SearchResultEntity extends Equatable {
   final String resultId;
   final String vehicleId;
   final String providerName;
-  final String providerLogoUrl;
-  final double providerRating;
-  final int providerRatingCount;
   final String vehicleType;
   final String vehicleName;
-  final String vehicleMake;
-  final String vehicleModel;
-  final String vehicleCategory;
   final String totalPriceAmount;
   final String totalPriceCurrency;
-  final String totalPriceDisplay;
   final bool bookable;
   final String vehicleImageUrl;
   final int maxPassengers;
   final int maxBags;
-  final int travelTimeMinutes;
-  final int waitTimeMinutesIncluded;
-  final String waitingMinutePriceDisplay;
-  final bool flightInfoRequired;
-  final double cancellationNoticeHours;
   final List<AmenityEntity> amenities;
-
 
   const SearchResultEntity({
     required this.resultId,
     required this.vehicleId,
     required this.providerName,
-    required this.providerLogoUrl,
-    required this.providerRating,
-    required this.providerRatingCount,
     required this.vehicleType,
     required this.vehicleName,
-    required this.vehicleMake,
-    required this.vehicleModel,
-    required this.vehicleCategory,
     required this.totalPriceAmount,
     required this.totalPriceCurrency,
-    required this.totalPriceDisplay,
     required this.bookable,
     required this.vehicleImageUrl,
     required this.maxPassengers,
     required this.maxBags,
-    required this.travelTimeMinutes,
-    required this.waitTimeMinutesIncluded,
-    required this.waitingMinutePriceDisplay,
-    required this.flightInfoRequired,
-    required this.cancellationNoticeHours,
     required this.amenities,
   });
 
@@ -112,26 +87,14 @@ class SearchResultEntity extends Equatable {
     resultId,
     vehicleId,
     providerName,
-    providerLogoUrl,
-    providerRating,
-    providerRatingCount,
     vehicleType,
     vehicleName,
-    vehicleMake,
-    vehicleModel,
-    vehicleCategory,
     totalPriceAmount,
     totalPriceCurrency,
-    totalPriceDisplay,
     bookable,
     vehicleImageUrl,
     maxPassengers,
     maxBags,
-    travelTimeMinutes,
-    waitTimeMinutesIncluded,
-    waitingMinutePriceDisplay,
-    flightInfoRequired,
-    cancellationNoticeHours,
     amenities,
   ];
 }
@@ -167,27 +130,16 @@ class CurrencyInfoEntity extends Equatable {
 class AmenityEntity extends Equatable {
   final String key;
   final String name;
-  final String description;
   final bool included;
   final bool chargeable;
-  final String priceDisplay;
 
   const AmenityEntity({
     required this.key,
     required this.name,
-    required this.description,
     required this.included,
     required this.chargeable,
-    required this.priceDisplay,
   });
 
   @override
-  List<Object?> get props => [
-    key,
-    name,
-    description,
-    included,
-    chargeable,
-    priceDisplay,
-  ];
+  List<Object?> get props => [key, name, included, chargeable];
 }
